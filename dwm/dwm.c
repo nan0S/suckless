9282ebd513ddbd941357150134b05640a79fdc92
Hubert Obrzut <skoczek133@gmail.com>
2021-04-19 22:36:30 +0200

    removed unused variables

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -937,8 +937,6 @@ void
 drawbar(Monitor *m)
 {
 	int x, w, sw = 0, n = 0, scm;
-	int boxs = drw->fonts->h / 9;
-	int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0;
 	Client *c;
 
