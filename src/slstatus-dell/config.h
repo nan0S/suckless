4170bc915d3b563255a53c6146a79cf140ff4e16
Hubert Obrzut <skoczek133@gmail.com>
2021-05-14 19:02:22 +0200

    added slstatus to dell

---
+++ src/slstatus-dell/config.h
@@ -0,0 +1,101 @@
+/* See LICENSE file for copyright and license details. */
+
+/* interval between updates (in ms) */
+const unsigned int interval = 1000;
+
+/* text to show if no value can be retrieved */
+// static const char unknown_str[] = "n/a";
+static const char unknown_str[] = "..";
+
+/* maximum output string length */
+#define MAXLEN 2048
+/* maximum output string of single command/function */
+#define COMMAND_MAXLEN 128
+/* maximum number of failures to print output */
+#define MAX_FAILURES 10
+
+/*
+ * function            description                     argument (example)
+ *
+ * battery_perc        battery percentage              battery name (BAT0)
+ *                                                     NULL on OpenBSD/FreeBSD
+ * battery_state       battery charging state          battery name (BAT0)
+ *                                                     NULL on OpenBSD/FreeBSD
+ * battery_remaining   battery remaining HH:MM         battery name (BAT0)
+ *                                                     NULL on OpenBSD/FreeBSD
+ * cpu_perc            cpu usage in percent            NULL
+ * cpu_freq            cpu frequency in MHz            NULL
+ * datetime            date and time                   format string (%F %T)
+ * disk_free           free disk space in GB           mountpoint path (/)
+ * disk_perc           disk usage in percent           mountpoint path (/)
+ * disk_total          total disk space in GB          mountpoint path (/")
+ * disk_used           used disk space in GB           mountpoint path (/)
+ * entropy             available entropy               NULL
+ * gid                 GID of current user             NULL
+ * hostname            hostname                        NULL
+ * ipv4                IPv4 address                    interface name (eth0)
+ * ipv6                IPv6 address                    interface name (eth0)
+ * kernel_release      `uname -r`                      NULL
+ * keyboard_indicators caps/num lock indicators        format string (c?n?)
+ *                                                     see keyboard_indicators.c
+ * keymap              layout (variant) of current     NULL
+ *                     keymap
+ * load_avg            load average                    NULL
+ * netspeed_rx         receive network speed           interface name (wlan0)
+ * netspeed_tx         transfer network speed          interface name (wlan0)
+ * num_files           number of files in a directory  path
+ *                                                     (/home/foo/Inbox/cur)
+ * ram_free            free memory in GB               NULL
+ * ram_perc            memory usage in percent         NULL
+ * ram_total           total memory size in GB         NULL
+ * ram_used            used memory in GB               NULL
+ * run_command         custom shell command            command (echo foo)
+ * swap_free           free swap in GB                 NULL
+ * swap_perc           swap usage in percent           NULL
+ * swap_total          total swap size in GB           NULL
+ * swap_used           used swap in GB                 NULL
+ * temp                temperature in degree celsius   sensor file
+ *                                                     (/sys/class/thermal/...)
+ *                                                     NULL on OpenBSD
+ *                                                     thermal zone on FreeBSD
+ *                                                     (tz0, tz1, etc.)
+ * uid                 UID of current user             NULL
+ * uptime              system uptime                   NULL
+ * username            username of current user        NULL
+ * vol_perc            OSS/ALSA volume in percent      mixer file (/dev/mixer)
+ * wifi_perc           WiFi signal in percent          interface name (wlan0)
+ * wifi_essid          WiFi ESSID                      interface name (wlan0)
+ */
+
+static const char eth_dev[] = "enp4s0";
+static const char wifi_dev[] = "wlp5s0";
+
+#define RESET 			"^c#FFFFFF^"
+#define VIOLET 			"^c#7A62DE^"
+#define LIGHT_BLUE 		"^c#46CDD0^"
+#define BLUE 			"^c#46A6D0^"
+#define GREEN 			"^c#3599A0^"
+#define YELLOW 			"^c#F4DFA0^"
+#define ORANGE 			"^c#F39C7B^"
+#define RED 			"^c#F04250^"
+#define PREFIX 			"| "
+#define SUFFIX 			" " RESET
+#define S 				*1000
+#define MS 				*1
+
+static struct arg args[] = {
+	/* function 		prefix 		color 		content 		suffix 		argument 				interval*/
+	{ run_command, 		" " 		VIOLET 		"Mail: %s" 		SUFFIX, 	"new-mails", 			1 S },
+	{ ipv4, 			PREFIX 		LIGHT_BLUE 	"ETH %s" 		SUFFIX, 	eth_dev, 				1 S },
+	{ wifi_essid, 		PREFIX 		BLUE 		"WIFI %s" 		SUFFIX, 	wifi_dev, 				1 S },
+	{ wifi_perc, 					BLUE		"%2s%%" 		SUFFIX, 	wifi_dev, 				1 S },
+	{ run_command, 		PREFIX 		GREEN 		"%s ", 						"get-speaker-name -s", 	1 S },
+	{ run_command, 		PREFIX 		GREEN		"VOL %s%%" 		SUFFIX, 	"volctl --get", 		0 },
+	{ run_command, 		PREFIX 		GREEN		"%s" 			SUFFIX, 	"muctl time", 			100 MS },
+	{ cpu_perc, 		PREFIX 		YELLOW 		"CPU %2s%%" 	SUFFIX, 	NULL, 					1 S },
+	{ ram_perc, 		PREFIX 		YELLOW 		"RAM %2s%%" 	SUFFIX, 	NULL, 					1 S },
+	{ battery_state, 	PREFIX 		ORANGE 		"BAT %s", 					"BAT0", 				15 S },
+	{ battery_perc, 							"%s%%" 			SUFFIX, 	"BAT0", 				15 S },
+	{ datetime, 		PREFIX 		RED 		"%s" 			SUFFIX, 	"%b %d (%a) %T", 		100 MS},
+};
+
---
+++ src/slstatus-dell/slstatus.c
@@ -0,0 +1,216 @@
+/* See LICENSE file for copyright and license details. */
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <X11/Xlib.h>
+
+#include "arg.h"
+#include "slstatus.h"
+#include "util.h"
+
+struct arg {
+	const char *(*func)();
+	const char *fmt;
+	const char *args;
+	const long interval;
+	char* buf;
+	struct timespec last_time;
+	int failures;
+};
+
+char buf[1024];
+static int done;
+static Display *dpy;
+
+#include "config.h"
+
+static void
+terminate(const int signo)
+{
+	(void)signo;
+
+	done = 1;
+}
+
+static void
+difftimespec(struct timespec *res, struct timespec *a, struct timespec *b)
+{
+	res->tv_sec = a->tv_sec - b->tv_sec - (a->tv_nsec < b->tv_nsec);
+	res->tv_nsec = a->tv_nsec - b->tv_nsec +
+	               (a->tv_nsec < b->tv_nsec) * 1E9;
+}
+
+static void
+usage(void)
+{
+	die("usage: %s [-s]", argv0);
+}
+
+static void
+refresh(const int signo)
+{
+	(void)signo;
+}
+
+static char status[MAXLEN];
+static char res_buf[MAXLEN];
+static size_t i, len;
+static int sflag, ret;
+const char* res;
+
+static int
+should_update(struct arg* arg) {
+	if (arg->interval == 0)
+		return 1;
+
+	struct timespec diff, now;
+	long ms;
+
+	clock_gettime(CLOCK_MONOTONIC, &now);
+	difftimespec(&diff, &now, &arg->last_time);
+	ms = diff.tv_sec * 1000 + diff.tv_nsec / 1e6;
+	
+	if (ms < arg->interval)
+		return 0;
+
+	arg->last_time = now;
+	return 1;
+}
+
+static void
+prepare_bar()
+{
+	char* cur = res_buf;
+	char* end = res_buf + sizeof(res_buf);
+
+	for (i = 0; i < LEN(args); i++) {
+		if (cur + COMMAND_MAXLEN <= end && args[i].interval > 0) {
+			args[i].buf = cur;
+			cur += COMMAND_MAXLEN;
+		}
+		else
+			args[i].buf = NULL;
+
+		args[i].failures = MAX_FAILURES + 1;
+	}
+}
+
+static void
+update_bar()
+{
+	size_t len;
+	status[0] = '\0';
+	for (i = len = 0; i < LEN(args); i++) {
+		if (should_update(&args[i])) {
+			if (!(res = args[i].func(args[i].args))) {
+				args[i].failures++;
+				res = unknown_str;
+			}
+			else
+				args[i].failures = 0;
+
+			if (args[i].buf)
+				strncpy(args[i].buf, res, COMMAND_MAXLEN);
+		}
+		else if (!(res = args[i].buf))
+			res = unknown_str;
+
+		if (args[i].failures <= MAX_FAILURES) {
+			if ((ret = esnprintf(status + len, sizeof(status) - len,
+							args[i].fmt, res)) < 0) {
+				break;
+			}
+			len += ret;
+		}
+	}
+
+	// ugly hack
+	if (status[0] == '|')
+		memmove(status, status + 1, sizeof(status));
+
+	if (sflag) {
+		puts(status);
+		fflush(stdout);
+		if (ferror(stdout))
+			die("puts:");
+	} else {
+		if (XStoreName(dpy, DefaultRootWindow(dpy), status)
+				   < 0) {
+			die("XStoreName: Allocation failed");
+		}
+		XFlush(dpy);
+	}
+}
+
+int
+main(int argc, char *argv[])
+{
+	struct sigaction act, hup;
+	struct timespec start, current, diff, intspec, wait;
+
+	sflag = 0;
+	ARGBEGIN {
+		case 's':
+			sflag = 1;
+			break;
+		default:
+			usage();
+	} ARGEND
+
+	if (argc) {
+		usage();
+	}
+
+	memset(&act, 0, sizeof(act));
+	act.sa_handler = terminate;
+	sigaction(SIGINT,  &act, NULL);
+	sigaction(SIGTERM, &act, NULL);
+
+	memset(&hup, 0, sizeof(hup));
+	hup.sa_handler = refresh;
+	sigaction(SIGHUP, &hup, NULL);
+
+	if (!sflag && !(dpy = XOpenDisplay(NULL))) {
+		die("XOpenDisplay: Failed to open display");
+	}
+
+	prepare_bar();
+
+	while (!done) {
+		if (clock_gettime(CLOCK_MONOTONIC, &start) < 0) {
+			die("clock_gettime:");
+		}
+		
+		update_bar();
+
+		if (!done) {
+			if (clock_gettime(CLOCK_MONOTONIC, &current) < 0) {
+				die("clock_gettime:");
+			}
+			difftimespec(&diff, &current, &start);
+
+			intspec.tv_sec = interval / 1000;
+			intspec.tv_nsec = (interval % 1000) * 1E6;
+			difftimespec(&wait, &intspec, &diff);
+
+			if (wait.tv_sec >= 0) {
+				if (nanosleep(&wait, NULL) < 0 &&
+				    errno != EINTR) {
+					die("nanosleep:");
+				}
+			}
+		}
+	}
+
+	if (!sflag) {
+		XStoreName(dpy, DefaultRootWindow(dpy), NULL);
+		if (XCloseDisplay(dpy) < 0) {
+			die("XCloseDisplay: Failed to close display");
+		}
+	}
+
+	return 0;
+}
