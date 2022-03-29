acc49c68d50f47cd9410b486849b8ad85898245e
Hubert Obrzut <skoczek133@gmail.com>
2022-03-29 19:23:33 +0200

    smaller st font

--- src/st/config.h
+++ src/st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode-Bold:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "FiraCode-Bold:pixelsize=14:antialias=true:autohint=true" };
+static char *font = "FiraCode-Bold:pixelsize=12:antialias=true:autohint=true";
+static char *font2[] = { "FiraCode-Bold:pixelsize=12:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
